80d67e1ed9124be659f7941400f3b7dfd9904ccc
shohag0007 <merinedrive007@gmail.com>
2024-08-11 23:32:59 +0600

    display food items on different tabs

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -28,15 +28,12 @@ const Menu = () => {
             {/* offered menu items*/}
             <MenuCategory items={offered}></MenuCategory>
             {/* dessert menu items*/}
-            <MenuCategory
-            items={desserts}
-            title="Dessert"
-            img={dessertImg}
+            <MenuCategory items={desserts} title="dessert" img={dessertImg}
             ></MenuCategory>
             <MenuCategory items={offered}></MenuCategory>
             <MenuCategory items={pizza} title={"pizza"} img={pizzaImg}></MenuCategory>
-            <MenuCategory items={salad} title={"Salad"} img={saladImg}></MenuCategory>
-            <MenuCategory items={soup} title={"Soup"} img={soupImg}></MenuCategory>
+            <MenuCategory items={salad} title={"salad"} img={saladImg}></MenuCategory>
+            <MenuCategory items={soup} title={"soup"} img={soupImg}></MenuCategory>
         </div>
     );
 };
--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -6,6 +6,7 @@ const NavBar = () => {
   const navOptions = <>
   <li ><Link  to="/">Home</Link></li>
   <li ><Link to="/menu">Our Menu</Link></li>
+  <li ><Link to="/order">Order Food</Link></li>
   
   <li></li>
 
